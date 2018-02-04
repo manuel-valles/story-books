95bf1f8ae71cbcfe0b9319a30b46a5b53cefe324
manukempo <manukempo@gmail.com>
2018-02-04 19:58:07 +0000

    Init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Import main modules
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initialize the application
+const app = express();
+
+// Index Route - Get request
+app.get('/', (req, res)=>{
+	res.send('Index');
+});
+
+// Port Varibale
+const port = process.env.PORT || 5000;
+
+// Listen in certain port and a callback function
+app.listen(port, ()=>{
+	console.log(`Server started on port ${port}`);
+});
