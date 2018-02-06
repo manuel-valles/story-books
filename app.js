71e42290ece11af1ffd4cc2e07a9ddf89a7743bf
manukempo <manukempo@gmail.com>
2018-02-06 19:56:47 +0000

    Story Model & Save story to MongoDB

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 
-// Load User Model
+// Load Models
 require('./models/User');
+require('./models/Story');
 
 // Passport Config
 require('./config/passport')(passport);
@@ -30,6 +32,10 @@ mongoose.connect(keys.mongoURI)
 
 // Initialize the application
 const app = express();
+
+// Body-Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
