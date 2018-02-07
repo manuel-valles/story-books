4acee3f2d689e916ff7f29dd70ae0c2dfdc9440f
Manuel Valles <manukempo@gmail.com>
2018-02-07 16:20:53 +0000

    Add and display Comments

--- routes/stories.js
+++ routes/stories.js
@@ -22,6 +22,7 @@ router.get('/show/:id', (req, res) => {
 		_id: req.params.id
 	})
 	.populate('user')
+	.populate('comments.commentUser')
 	.then(story => {
 		res.render('stories/show', {
 			story: story
@@ -103,6 +104,25 @@ router.delete('/:id', (req, res) => {
 		.then(() => {
 			res.redirect('/dashboard');
 		});
+});
+
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+	Story.findOne({_id: req.params.id})
+	.then(story => {
+		const newComment = {
+			commentBody: req.body.commentBody,
+			commentUser: req.user.id
+		}
+
+		// Add to comments array
+		story.comments.unshift(newComment);
+
+		story.save()
+			.then(story => {
+				res.redirect(`/stories/show/${story.id}`);
+			});
+	});
 });
 
 module.exports = router;
